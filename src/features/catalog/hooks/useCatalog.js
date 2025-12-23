81f762626206a7949a3e5c2d5cfc07c9d570437c
Ricardo Mesa <rmesaf@gmail.com>
2025-12-22 23:02:59 -0500

    Google books public api data

---
+++ src/features/catalog/hooks/useCatalog.js
@@ -0,0 +1,31 @@
+// Packages
+import useSWR from "swr";
+
+// App
+import fetchGoogleBooksData from "features/catalog/api/googleBooks.api.js";
+
+function useCatalog(config, swrOptions = {}) {
+    console.log('useCatalog called with config:', config);
+    const query = config?.query;
+    const page = config?.page ?? 0;
+    const pageSize = config?.pageSize;
+
+    const { data, error, isLoading } = useSWR(
+        ['catalog', "googleBooks", { query, page, pageSize }],
+        ([, , args]) => fetchGoogleBooksData({ ...args }),
+        {
+            revalidateOnFocus: false,
+            revalidateIfStale: false,
+            revalidateOnReconnect: false,
+            ...swrOptions,
+        }
+    );
+
+    return {
+        books: data ?? [],
+        isLoading,
+        error,
+    };
+}
+
+export default useCatalog;
