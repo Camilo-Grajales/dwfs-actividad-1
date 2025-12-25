0eda33373bb001596dbaae37563fa253cbf8dd76
Ricardo Mesa <rmesaf@gmail.com>
2025-12-25 16:00:50 -0500

    [FEAT] - Cart Context

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,20 @@ import React from 'react';
 import Header from 'shared/components/Header';
 import Footer from 'shared/components/Footer';
 import AppRouter from 'router';
+import { CartProvider } from 'features/cart/context/CartProvider';
 
 // Styles
 import './App.scss'
 
 function App() {
   return (
-    <>
+    <CartProvider>
       <Header />
       <main className='app-main'>
         <AppRouter />
       </main>
       <Footer />
-    </>
+    </CartProvider>
   )
 }
 
---
+++ src/features/cart/context/cartReducer.js
@@ -0,0 +1,54 @@
+export const CART_ACTIONS = {
+    ADD_ITEM: "ADD_ITEM",
+    CLEAR_CART: "CLEAR_CART",
+    REMOVE_ITEM: "REMOVE_ITEM",
+    UPDATE_QUANTITY: "UPDATE_QUANTITY",
+};
+
+export const INITIAL_CART_STATE = {
+    items: [],
+};
+
+export function cartReducer(state, action) {
+    switch (action.type) {
+        case CART_ACTIONS.ADD_ITEM: {
+            const item = action.payload;
+            const existingItem = state.items.find(i => i.id === item.id);
+            if (existingItem) {
+                return state;
+            }
+            return {
+                ...state,
+                items: [...state.items, item],
+            };
+        }
+
+        case CART_ACTIONS.CLEAR_CART: {
+            return INITIAL_CART_STATE;
+        }
+
+        case CART_ACTIONS.REMOVE_ITEM: {
+            const { id } = action.payload;
+            return {
+                ...state,
+                items: state.items.filter(item => item.id !== id),
+            };
+        }
+
+        case CART_ACTIONS.UPDATE_QUANTITY: {
+            const { id, quantity } = action.payload;
+
+            return {
+                ...state,
+                items: state.items.map(item =>
+                    item.id === id
+                        ? { ...item, quantity }
+                        : item
+                ),
+            };
+        }
+
+        default:
+            return state;
+    }
+};
